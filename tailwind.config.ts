ce4db23979faaa0de6df1081c035fcbf7cc7748b
Noah Haubenreiser <noah.haubenreiser@gmail.com>
2023-12-12 11:27:45 +0100

    Updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#000000",
+                secondary: "#A9A9A9",
+            },
+            boxShadow: {
+                button: "0px 4px 0px 0px #C0C0C0",
+            },
+            fontSize: {
+                title: "96px",
+                headline: "32px",
+            },
+            margin: {
+                "4px": "4px",
+                "26px": "26px",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+export default config;
