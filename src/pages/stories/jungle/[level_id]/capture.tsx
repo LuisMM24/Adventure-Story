12f56ad705616d50e0b186628194c0ade717bee3
Noah Haubenreiser <noah.haubenreiser@gmail.com>
2023-12-14 12:44:02 +0100

    Added image compression

--- src/pages/stories/jungle/[level_id]/capture.tsx
+++ src/pages/stories/jungle/[level_id]/capture.tsx
@@ -5,6 +5,7 @@ import { ReaderService } from "@/utils/reader";
 import { Message } from "ai";
 import { useRouter } from "next/router";
 import React from "react";
+import Compressor from "compressorjs";
 
 type Props = {};
 
@@ -29,35 +30,46 @@ function Capture(props: Props) {
         if (event.target.files && event.target.files[0]) {
             const file = event.target.files[0];
             if (file) {
-                ReaderService({
-                    file,
-                    onload: async (event) => {
-                        if (event.target && typeof event.target.result === "string") {
-                            const fileData = event.target.result;
+                // STEP 2: Compress the file
+                new Compressor(file, {
+                    quality: 0.6,
+                    convertSize: 5,
+                    success(compressedFile) {
+                        console.log("Compressed from", file.size, "to", compressedFile.size);
+                        ReaderService({
+                            file: compressedFile as File,
+                            onload: async (event) => {
+                                if (event.target && typeof event.target.result === "string") {
+                                    const fileData = event.target.result;
 
-                            const response = await AxiosService({
-                                endpoint: "story/answer",
-                                data: JSON.stringify(fileData),
-                                method: "post",
-                            });
+                                    const response = await AxiosService({
+                                        endpoint: "story/answer",
+                                        data: JSON.stringify(fileData),
+                                        method: "post",
+                                    });
 
-                            const levelAnswer: string = response.data;
+                                    const levelAnswer: string = response.data;
 
-                            // STEP 2: Save level answer to user state
-                            const answerMessage: Message = {
-                                id: `${currentLevel?.id}-answer`,
-                                role: "user",
-                                content: `The solution to the last challenge is: ${levelAnswer}`,
-                            };
-                            const updatedLevels = [
-                                ...user.jungleStory.levels.filter((i) => i.id !== levelId),
-                                { id: levelId, aiMessage: currentLevel?.aiMessage ?? null, answerMessage: answerMessage, success: true },
-                            ].sort((a, b) => a.id - b.id);
+                                    // STEP 3: Save level answer to user state
+                                    const answerMessage: Message = {
+                                        id: `${currentLevel?.id}-answer`,
+                                        role: "user",
+                                        content: `The solution to the last challenge is: ${levelAnswer}`,
+                                    };
+                                    const updatedLevels = [
+                                        ...user.jungleStory.levels.filter((i) => i.id !== levelId),
+                                        { id: levelId, aiMessage: currentLevel?.aiMessage ?? null, answerMessage: answerMessage, success: true },
+                                    ].sort((a, b) => a.id - b.id);
 
-                            dispatch({ type: "SET_USER", userData: { name: user.name, jungleStory: { levels: updatedLevels } } });
+                                    dispatch({ type: "SET_USER", userData: { name: user.name, jungleStory: { levels: updatedLevels } } });
 
-                            console.log(`Answer for level ${levelId} saved: ${levelAnswer}`);
-                        }
+                                    console.log(`Answer for level ${levelId} saved: ${levelAnswer}`);
+                                }
+                            },
+                        });
+                    },
+                    error(err) {
+                        console.log(err.message);
                     },
                 });
             }
