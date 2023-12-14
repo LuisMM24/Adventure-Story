a5c7fd973c85616144f077d9c9f7adebfaa0ac53
luismm24 <luis@mvst.co>
2023-12-14 15:37:12 +0100

    remove: Comments

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -26,8 +26,3 @@ export const Button = (props: PropsWithChildren<Props>) => {
     </button>
   );
 };
-{
-  /* <div className="rounded-[8px] bg-button shadow-inner-button w-inner-button h-inner-button absolute top-[3px] right-[3px] font-Digitalt flex items-center justify-center">
-       
-       </div> */
-}
